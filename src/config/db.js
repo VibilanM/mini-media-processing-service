0692ee491dd1b0623c8d36419eb3d476785e4a5d
Vibilan <vibilan2793@gmail.com>
2026-07-15 08:25:17 +0530

    db configuration

---
+++ src/config/db.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const uri = process.env.MONGODB_URI;
+        if (!uri) {
+            throw new Error("MONGO_URI or MONGODB_URI is not defined in environment variables");
+        }
+        await mongoose.connect(uri);
+    }
+    catch (error) {
+        console.error("MongoDB Connection Error: ", error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
