27abe26934c98712bc214b037486aec10f4b4203
Vibilan <vibilan2793@gmail.com>
2026-07-23 08:02:49 +0530

    dummy worker debugged

--- src/workers/videoWorker.js
+++ src/workers/videoWorker.js
@@ -1,5 +1,10 @@
 import { Worker } from "bullmq";
 import Video from "../models/videoModel.js";
+import dotenv from "dotenv";
+import connectDB from "../config/db.js";
+
+dotenv.config();
+connectDB();
 
 const worker = new Worker(
     "video-processing",
@@ -12,17 +17,16 @@ const worker = new Worker(
 
         console.log("Starting processing for job: ", videoId);
 
-        for (let i = 0; i <= 100; i++) {
-            await Video.findByIdAndUpdate(videoId, {
-                status: `processing ${i}%`
-        });
-            
+        for (let i = 20; i <= 100; i += 20) {
+            await new Promise(resolve => setTimeout(resolve, 2000));
+            console.log(`Processing ${i}%`);
+        }
+
         await Video.findByIdAndUpdate(videoId, {
             status: "ready"
         });
 
         console.log("Processing completed for job: ", videoId);
-        }
     },
     {
         connection: {
