d9ab009cba2ee6a845e81c78d5be10336b40c1d8
Vibilan <vibilan2793@gmail.com>
2026-08-04 12:18:38 +0530

    upload and download file functions

---
+++ src/storage/objectStorage.service.js
@@ -0,0 +1,21 @@
+import minioClient from "./minio.client.js";
+import fs from "node:fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const BUCKET = process.env.MINIO_BUCKET;
+
+async function uploadFile(filePath, objectKey) {
+    const fileStream = fs.createReadStream(filePath);
+    const stat = fs.statSync(filePath);
+
+    await minioClient.putObject(BUCKET, objectKey, fileStream, stat.size);
+    return objectKey;
+}
+
+async function downloadFile(objectKey, localPath) {
+    await minioClient.fGetObject(BUCKET, objectKey, localPath);
+    return localPath;
+}
+
