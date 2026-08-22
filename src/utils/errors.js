9db9d3e7ff6fd5e5e053ec0c5daf8ede5e28e791
Vibilan <vibilan2793@gmail.com>
2026-08-22 14:19:08 +0530

    classification of errors

---
+++ src/utils/errors.js
@@ -0,0 +1,51 @@
+class NonRetryableError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "NonRetryableError";
+    }
+}
+
+class RetryableError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "RetryableError";
+    }
+}
+
+function classifyError(error) {
+    const message = error.message?.toLowerCase() || "";
+
+    const permanentPatterns = [
+        "invalid data found",
+        "no such file or directory",
+        "moov atom not found",
+        "unrecognized option",
+        "invalid argument",
+        "cast to objectid failed",
+    ];
+
+    for (const pattern of permanentPatterns) {
+        if (message.includes(pattern)) {
+            return "permanent";
+        }
+    }
+
+    const transientPatterns = [
+        "econnrefused",
+        "econnreset",
+        "etimedout",
+        "socket hand up",
+        "replica set",
+        "enomem",
+    ]
+
+    for (const pattern of transientPatterns) {
+        if (message.includes(patterns)) {
+            return "transient";
+        }
+    }
+
+    return "unknown";
+}
+
+export { NonRetryableError, RetryableError, classifyError };
